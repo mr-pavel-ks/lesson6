1df53826ac90afd2399be7178f27ef13c4ffcbb6
Pavel Korkus <mr.pavel.ks@gmail.com>
2020-08-26 20:20:20 +0500

    2v

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class Animal():
     def feed(self):
         print('Животное сыто')
 
-    def __init__(self):
-        self.name = []
-        self.weight = 0
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
 
     def sound(self):
         print(f"Животное издает звук  {self.voice}")
@@ -27,18 +27,10 @@ class Goose(Birds):
     voice = 'gagagaga'
     type_an = 'Гусь'
 
-    def __init__(self):
-        self.name = ['Серый', 'Белый']
-        self.weight = 3
-
 
 class Cow(MilkyWay):
     voice = 'muuuuuuu'
     type_an = 'Корова'
-
-    def __init__(self):
-        self.name = ['Манька']
-        self.weight = 50
 
 
 class Sheep(Animal):
@@ -48,51 +40,44 @@ class Sheep(Animal):
     def cut(self):
         print('Животное подстрижено')
 
-    def __init__(self):
-        self.name = ['Барашек', 'Кудрявый']
-        self.weight = 20
-
 
 class Chicken(Birds):
     voice = 'kokoko'
     type_an = 'Курица'
-
-    def __init__(self):
-        self.name = ["Ко-Ко", "Кукареку"]
-        self.weight = 5
 
 
 class Goat(MilkyWay):
     voice = 'mememe'
     type_an = 'Коза'
 
-    def __init__(self):
-        self.name = ["Рога", "Копыта"]
-        self.weight = 15
-
 
 class Duck(Birds):
     voice = 'Crya'
     type_an = 'Утка'
 
-    def __init__(self):
-        self.name = ["Кряква"]
-        self.weight = 5
 
+my_Goose_1 = Goose('Серый', 5)
+my_Goose_2 = Goose('Белый', 15)
+my_Cow = Cow('Манька', 30)
+my_Sheep_1 = Sheep('Барашек', 10)
+my_Sheep_2 = Sheep('Кудрявый', 11)
+my_Chicken_1 = Chicken("Ко-Ко", 16)
+my_Chicken_2 = Chicken("Кукареку", 5)
+my_Goat_1 = Goat("Рога", 6)
+my_Goat_2 = Goat("Копыта", 2)
+my_Duck = Duck('Кряква', 1)
 
-my_Goose = Goose()
-my_Cow = Cow()
-my_Sheep = Sheep()
-my_Chicken = Chicken()
-my_Goat = Goat()
-my_Duck = Duck()
 oll = []
-animal_list = [my_Goose, my_Cow, my_Sheep, my_Chicken, my_Goat, my_Duck]
+animal_list = [my_Goose_1, my_Goose_2, my_Cow, my_Sheep_1, my_Sheep_2, my_Chicken_1, my_Chicken_2, my_Goat_1, my_Goat_2,
+               my_Duck]
+
 for an_weight in animal_list:
-    oll_weight = an_weight.weight * len(an_weight.name)
-    oll.append(oll_weight)
+    oll.append(an_weight.weight)
     max_weight = max(oll)
     if max_weight == an_weight.weight:
-        print(f' Самое тяжелое животное: {an_weight.type_an}')
+        name_big_animal = an_weight.name
+        type_big_animal = an_weight.type_an
+print(f' Самое тяжелое животное: {type_big_animal}, {name_big_animal}')
 print(f" Общий вес животных: {sum(oll)}")
+
 
